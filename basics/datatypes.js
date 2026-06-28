0b52b8258ce1474bf322cfc460ce312a386b0687
Anukriti2028 <153358037+Anukriti2028@users.noreply.github.com>
2026-06-28 07:47:23 +0000

    variables and data types

---
+++ basics/datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+/*
+The javascript engine is hidden inside the browser but we are
+not writting our code on browser , we are writting on the
+node.js so alert(3+3) will give an error but if we run the same alert statement on console 
+it will print 6.
+*/
+
+let name ="hitesh"
+let age = 18
+let isLoggedIn = false
+console.log(typeof age);
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
+// Primitive data types : number , bignint , string , boolean(true,false),null(standalone value), undefined,symbol(unique)
+//Non-primitive data types : objects
+/*
+
+ */
+/*
+undefined means you have not defined the value , we have only declared the 
+variable and null is the representation of empty value means 
+we are intentionally saying that is it empty.
+*/
+
+
---
+++ basics/variables1.js
@@ -0,0 +1,14 @@
+const accountID = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity ="Jaipur"
+let accountState;
+
+// accountID = 2 // const cannot be changed not allowed
+console.log(accountID);
+accountEmail ="hc@hc.com"
+accountPassword="21212121"
+accountCity="Bengaluru"
+
+console.table([accountID,accountEmail,accountPassword,accountCity,accountState])
+
