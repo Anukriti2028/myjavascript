c1bb3e62bfdc869da35072bb290ee15ae9b3d76d
Anukriti2028 <153358037+Anukriti2028@users.noreply.github.com>
2026-07-13 12:22:57 +0000

    api

---
+++ basics2/objects2.js
@@ -0,0 +1,53 @@
+// const tinderUser = {}
+// tinderUser.id = "123abc"
+// tinderUser.name = "Sammy"
+// tinderUser.isLoggedIn = false
+// console.log(tinderUser)
+
+// const regularUser = {
+//     email :"Some@gmail.com",
+//     fullname :{
+//         userFullname:{
+//             firstname: "hitesh",
+//             lastname:"choudhary"
+//         }
+//     }
+// }
+// console.log(regularUser.fullname)
+// const obj1={
+//     1:"a",
+//     2:"b"
+// }
+// const obj2={
+//     3:"a",
+//     4:"b"
+// }
+// // const obj3={obj1,obj2}
+// // console.log(obj3)
+
+// // const obj3 = Object.assign({},obj1,obj2)
+
+// const obj3 = {...obj1, ...obj2}
+// console.log(obj3)
+
+// const users = [
+//     {
+//         id:1,
+//         email:"h@gmail.com"
+//     }
+// ]
+// users[1].email
+// console.log(tinderUser)
+// console.log(Object.keys(tinderUser))
+// console.log(Object.values(tinderUser))
+// console.log(Object.entries(tinderUser))
+// console.log(tinderUser.hasOwnProperty('isLoggedIn'))
+
+const course = {
+    coursename : "js in hindi",
+    price: "999",
+    courseInstructor : "hitesh"
+}
+const {courseInstructor : instructor}=course
+console.log(instructor)
+
